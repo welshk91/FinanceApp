2b4869446374be50cb9fa2fa08a3ce1eb37caaa2
Kevin Welsh <welshk91@gmail.com>
2012-10-24 18:36:15 -0400

    Added a simple ActionBarSherlock to main screen

--- src/com/databases/example/Main.java
+++ src/com/databases/example/Main.java
@@ -1,10 +1,10 @@
 package com.databases.example;
 
 import java.util.ArrayList;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources.Theme;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -12,8 +12,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
+import com.actionbarsherlock.app.SherlockActivity;
 
-public class Main extends FragmentActivity {	
+public class Main extends SherlockActivity {	
 	int page;
 
 	//Variables for the Views
@@ -114,6 +115,8 @@ public class Main extends FragmentActivity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		//setTheme(R.style.Sherlock___Theme);
+		//android:theme="@android:style/Theme.Holo"
 		super.onCreate(savedInstanceState);
 		page = R.layout.main;
 		setContentView(R.layout.main);

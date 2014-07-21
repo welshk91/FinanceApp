aecf7d19ee543085822a4af21c59af3b7190fed3
Kevin Welsh <welshk91@gmail.com>
2014-07-21 12:51:29 -0700

    Cleaned up Dropbox file

--- financeApp/src/main/java/com/databases/example/Dropbox.java
+++ financeApp/src/main/java/com/databases/example/Dropbox.java
@@ -5,11 +5,14 @@
 
 package com.databases.example;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.channels.FileChannel;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuItem;
@@ -22,208 +25,199 @@ import com.dropbox.sync.android.DbxFile;
 import com.dropbox.sync.android.DbxFileSystem;
 import com.dropbox.sync.android.DbxPath;
 
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.Toast;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 public class Dropbox extends SherlockFragmentActivity{
-	private DbxAccountManager dbAccountManager;
-	private static final int REQUEST_LINK_TO_DBX = 100;
-	private static final int DBX_CHOOSER_REQUEST = 200;
-	private static final String appKey = "n98lux9z2rp08lb";
-	private static final String appSecret = "exp7ofyw3illtlw";
+    private DbxAccountManager dbAccountManager;
+    private static final int REQUEST_LINK_TO_DBX = 100;
+    private static final int DBX_CHOOSER_REQUEST = 200;
+    private static final String appKey = "n98lux9z2rp08lb";
+    private static final String appSecret = "exp7ofyw3illtlw";
 
-	@Override
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.dropbox);
-		setTitle("Dropbox");
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dropbox);
+        setTitle("Dropbox");
 
-		getSupportActionBar().setHomeButtonEnabled(true);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		//Initialize DropBox Account Manager
-		dbAccountManager = DbxAccountManager.getInstance(getApplicationContext(), appKey, appSecret);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-		//Display Login Status
-		dropboxStatus();
+        //Initialize DropBox Account Manager
+        dbAccountManager = DbxAccountManager.getInstance(getApplicationContext(), appKey, appSecret);
 
-	}//end onCreate
+        //Display Login Status
+        dropboxStatus();
 
-	//Update Status
-	public void dropboxStatus(){
-		TextView tvLogStatus = (TextView)findViewById(R.id.TextViewLogStatus);
-		Button bLogStatus = (Button)findViewById(R.id.ButtonLogin);
+    }//end onCreate
 
-		if(dbAccountManager.hasLinkedAccount()==true){
-			tvLogStatus.setText("Status: Logged In");	
-			bLogStatus.setText("Log Out");
-		}
-		else{
-			tvLogStatus.setText("Status: Logged Out");
-			bLogStatus.setText("Log In");
-		}
-	}
+    //Update Status
+    public void dropboxStatus(){
+        TextView tvLogStatus = (TextView)findViewById(R.id.TextViewLogStatus);
+        Button bLogStatus = (Button)findViewById(R.id.ButtonLogin);
 
-	//Login or out
-	public void dropboxLogin(View v){
-		if(dbAccountManager.hasLinkedAccount()==false){
-			dbAccountManager.startLink(this, REQUEST_LINK_TO_DBX);
-		}
-		else{
-			dbAccountManager.unlink();
-			dropboxStatus();
-		}
-	}
+        if(dbAccountManager.hasLinkedAccount()){
+            tvLogStatus.setText("Status: Logged In");
+            bLogStatus.setText("Log Out");
+        }
+        else{
+            tvLogStatus.setText("Status: Logged Out");
+            bLogStatus.setText("Log In");
+        }
+    }
 
-	//Fires Up Dropbox Chooser
-	public void dropboxChooser(View v){
-		DbxChooser mChooser = new DbxChooser(appKey);
-		mChooser.forResultType(DbxChooser.ResultType.FILE_CONTENT).launch(this, DBX_CHOOSER_REQUEST);
-	}
+    //Login or out
+    public void dropboxLogin(View v){
+        if(!dbAccountManager.hasLinkedAccount()){
+            dbAccountManager.startLink(this, REQUEST_LINK_TO_DBX);
+        }
+        else{
+            dbAccountManager.unlink();
+            dropboxStatus();
+        }
+    }
 
-	//Restores database file from Dropbox Chooser
-	public void dropboxRestore(Result result){
-		DatabaseHelper dh = new DatabaseHelper(this);
-		String restoreDBPath = result.getLink().getPath();
-		File currentDB = dh.getDatabase();
-		File restoreDB = new File(restoreDBPath);
+    //Fires Up Dropbox Chooser
+    public void dropboxChooser(View v){
+        DbxChooser mChooser = new DbxChooser(appKey);
+        mChooser.forResultType(DbxChooser.ResultType.FILE_CONTENT).launch(this, DBX_CHOOSER_REQUEST);
+    }
 
-		//Write restore file into current database file
-		try{
-			FileChannel src = new FileInputStream(restoreDB).getChannel();
-			FileChannel dst = new FileOutputStream(currentDB).getChannel();
-			dst.transferFrom(src, 0, src.size());
-			src.close();
-			dst.close();
-			Log.d("Dropbox-DropboxRestore", "Successfully restored database to " + restoreDB.getAbsolutePath());
-			Toast.makeText(this, "You restored from \n" + restoreDB.getAbsolutePath(), Toast.LENGTH_LONG).show();
-		} catch(Exception e){
-			Log.e("Dropbox-DropboxRestore", "Restore failed \n" + e);
-			Toast.makeText(this, "Restore failed \n" + e, Toast.LENGTH_LONG).show();
-		}
-	}
+    //Restores database file from Dropbox Chooser
+    public void dropboxRestore(Result result){
+        DatabaseHelper dh = new DatabaseHelper(this);
+        String restoreDBPath = result.getLink().getPath();
+        File currentDB = dh.getDatabase();
+        File restoreDB = new File(restoreDBPath);
 
-	//Dropbox Drop-In "Saver"
-	public void dropboxBackup(View v){
-		Toast.makeText(this, "Coming Soon...", Toast.LENGTH_SHORT).show();
-	}
+        //Write restore file into current database file
+        try{
+            FileChannel src = new FileInputStream(restoreDB).getChannel();
+            FileChannel dst = new FileOutputStream(currentDB).getChannel();
+            dst.transferFrom(src, 0, src.size());
+            src.close();
+            dst.close();
+            Log.d("Dropbox-DropboxRestore", "Successfully restored database to " + restoreDB.getAbsolutePath());
+            Toast.makeText(this, "You restored from \n" + restoreDB.getAbsolutePath(), Toast.LENGTH_LONG).show();
+        } catch(Exception e){
+            Log.e("Dropbox-DropboxRestore", "Restore failed \n" + e);
+            Toast.makeText(this, "Restore failed \n" + e, Toast.LENGTH_LONG).show();
+        }
+    }
 
-	//When the sync button is pressed
-	public void dropboxSync(View v) throws DbxException {
-		DbxFileSystem dbFileSystem = null;
+    //Dropbox Drop-In "Saver"
+    public void dropboxBackup(View v){
+        Toast.makeText(this, "Coming Soon...", Toast.LENGTH_SHORT).show();
+    }
 
-		try {
-			dbFileSystem = DbxFileSystem.forAccount(dbAccountManager.getLinkedAccount());
-		} catch (Unauthorized e) {
-			Toast.makeText(this, "Unauthorized to use Dropbox account", Toast.LENGTH_LONG).show();
-			Log.e("Dropbox-dropboxSync", "Unauthorized to use dropbox account? e = "+e);
-			e.printStackTrace();
-			return;
-		}
-		catch(Exception e){
-			Log.e("Dropbox-dropboxSync", "Are you logged in? Error e ="+e);
-			e.printStackTrace();
-			Toast.makeText(this, "Not Logged In", Toast.LENGTH_SHORT).show();
-			return;
-		}
+    //When the sync button is pressed
+    public void dropboxSync(View v) throws DbxException {
+        DbxFileSystem dbFileSystem;
 
-		//Make the sync file (should be the current database)
-		DbxPath syncFilePath = new DbxPath("/Sync/dbSync");
-		DbxFile syncFile = null;
+        try {
+            dbFileSystem = DbxFileSystem.forAccount(dbAccountManager.getLinkedAccount());
+        } catch (Unauthorized e) {
+            Toast.makeText(this, "Unauthorized to use Dropbox account", Toast.LENGTH_LONG).show();
+            Log.e("Dropbox-dropboxSync", "Unauthorized to use dropbox account? e = "+e);
+            e.printStackTrace();
+            return;
+        }
+        catch(Exception e){
+            Log.e("Dropbox-dropboxSync", "Are you logged in? Error e ="+e);
+            e.printStackTrace();
+            Toast.makeText(this, "Not Logged In", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-		//Need to force sync to stop "conflicting copy" bugs
-		//dbFileSystem.syncNowAndWait();
+        //Make the sync file (should be the current database)
+        DbxPath syncFilePath = new DbxPath("/Sync/dbSync");
+        DbxFile syncFile;
 
-		try {
-			syncFile = dbFileSystem.open(syncFilePath);
-			Log.e("Dropbox-dropboxSync", "Opened Sync File successfully");
-		} catch(DbxException.NotFound e){
-			Log.e("Dropbox-dropboxSync", "File not found? e = "+e);
-			e.printStackTrace();
-			Log.e("Dropbox-dropboxSync", "Create a new file");
-			syncFile = dbFileSystem.create(syncFilePath);
-		} catch (DbxException e) {
-			Log.e("Dropbox-dropboxSync", "Error opening file. e = "+e);
-			e.printStackTrace();
-			Toast.makeText(this, "Error opening file for dropbox syncing", Toast.LENGTH_LONG).show();
-			return;
-		}
+        //Need to force sync to stop "conflicting copy" bugs
+        //dbFileSystem.syncNowAndWait();
 
-		//Write current database into the sync file
-		DatabaseHelper dh = new DatabaseHelper(this);
-		File currentDB = dh.getDatabase();
-		try {
-			syncFile.writeFromExistingFile(currentDB, false);
-			Log.e("Dropbox-dropboxSync", "Synced File successfully");
-			Toast.makeText(this, "Synced File successfully", Toast.LENGTH_LONG).show();
-		} catch (IOException e) {
-			Log.e("Dropbox-dropboxSync", "I/O Error syncing file. e = "+e);
-			e.printStackTrace();
-			Toast.makeText(this, "I/O Error syncing file", Toast.LENGTH_LONG).show();
-			return;
-		}
-		catch(DbxFile.StreamExclusionException e){
-			Log.e("Dropbox-dropboxSync", "Read/Write stream already opened? e = "+e);
-			e.printStackTrace();
-			Toast.makeText(this, "Read/Write stream already opened?", Toast.LENGTH_LONG).show();
-			return;
-		} finally{
-			syncFile.close();
-		}
-	}
+        try {
+            syncFile = dbFileSystem.open(syncFilePath);
+            Log.e("Dropbox-dropboxSync", "Opened Sync File successfully");
+        } catch(DbxException.NotFound e){
+            Log.e("Dropbox-dropboxSync", "File not found? e = "+e);
+            e.printStackTrace();
+            Log.e("Dropbox-dropboxSync", "Create a new file");
+            syncFile = dbFileSystem.create(syncFilePath);
+        } catch (DbxException e) {
+            Log.e("Dropbox-dropboxSync", "Error opening file. e = "+e);
+            e.printStackTrace();
+            Toast.makeText(this, "Error opening file for dropbox syncing", Toast.LENGTH_LONG).show();
+            return;
+        }
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == REQUEST_LINK_TO_DBX) {
-			if (resultCode == SherlockFragmentActivity.RESULT_OK) {
-				dropboxStatus();
-				Log.d("Dropbox-onActivityResult","Logged In/Out Successfully To Dropbox");
+        //Write current database into the sync file
+        DatabaseHelper dh = new DatabaseHelper(this);
+        File currentDB = dh.getDatabase();
+        try {
+            syncFile.writeFromExistingFile(currentDB, false);
+            Log.e("Dropbox-dropboxSync", "Synced File successfully");
+            Toast.makeText(this, "Synced File successfully", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Log.e("Dropbox-dropboxSync", "I/O Error syncing file. e = "+e);
+            e.printStackTrace();
+            Toast.makeText(this, "I/O Error syncing file", Toast.LENGTH_LONG).show();
+        }
+        catch(DbxFile.StreamExclusionException e){
+            Log.e("Dropbox-dropboxSync", "Read/Write stream already opened? e = "+e);
+            e.printStackTrace();
+            Toast.makeText(this, "Read/Write stream already opened?", Toast.LENGTH_LONG).show();
+        } finally{
+            syncFile.close();
+        }
+    }
 
-			} else {
-				//Link failed or was cancelled by the user.
-				Log.e("Dropbox-onActivityResult","Result FAILED. Cant use dropbox");
-			}
-		} 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_LINK_TO_DBX) {
+            if (resultCode == SherlockFragmentActivity.RESULT_OK) {
+                dropboxStatus();
+                Log.d("Dropbox-onActivityResult","Logged In/Out Successfully To Dropbox");
 
-		if (requestCode == DBX_CHOOSER_REQUEST) {
-			if (resultCode == SherlockFragmentActivity.RESULT_OK) {
-				DbxChooser.Result result = new DbxChooser.Result(data);
-				Log.d("Dropbox-onActivityResult", "Link to selected file: " + result.getLink());
-				dropboxRestore(result);
-			} 
+            } else {
+                //Link failed or was cancelled by the user.
+                Log.e("Dropbox-onActivityResult","Result FAILED. Cant use dropbox");
+            }
+        }
 
-			else {
-				// Failed or was cancelled by the user.
-				Log.d("Dropbox-onActivityResult", "Dropbox Chooser: Failed or Canceled");
-				Toast.makeText(this, "Failed or canceled", Toast.LENGTH_LONG).show();
-			}
-		}
+        if (requestCode == DBX_CHOOSER_REQUEST) {
+            if (resultCode == SherlockFragmentActivity.RESULT_OK) {
+                DbxChooser.Result result = new DbxChooser.Result(data);
+                Log.d("Dropbox-onActivityResult", "Link to selected file: " + result.getLink());
+                dropboxRestore(result);
+            }
 
-		else {
-			super.onActivityResult(requestCode, resultCode, data);
-		}
+            else {
+                // Failed or was cancelled by the user.
+                Log.d("Dropbox-onActivityResult", "Dropbox Chooser: Failed or Canceled");
+                Toast.makeText(this, "Failed or canceled", Toast.LENGTH_LONG).show();
+            }
+        }
 
-	}
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
 
-	//For Menu Items
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:    
-	        NavUtils.navigateUpFromSameTask(this);
-			break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
+    }
+
+    //For Menu Items
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }//end of Dropbox
